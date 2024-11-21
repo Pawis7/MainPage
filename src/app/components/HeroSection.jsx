6828b799ea70e8213af950575f995aedafb8426b
Pawis <diego223624@gmail.com>
2024-11-20 21:10:24 -0600

    more things

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,33 +1,62 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
-        <div className='grid grid-cols-1 md:grid-cols-12'> 
-            <div className='col-span-7 place-self-center'>
-                <h1 className= "text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-                    I´m Pawis!
-                </h1>
-                <p className='text-[#ffffff] text-lg'>
-                    i´m making my first web site with webcoded tutorial
-                </p>
-            </div>
-            <div className='col-span-5'>
-                <div className='rounded-full bg-[#080808] w-[400px] h-[400px] relative'>
-                    <Image
-                        src="/images/hero.png"
-                        alt='hero'
-                        className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                        width={500}
-                        height={500}
-                    />
-                </div>
-            </div>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-indigo-600">
+              {" "}
+              Pawis{" "}
+            </span>
+            Here!
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Web Developer",
+                1000,
+                "Front end developer",
+                1000,
+                "c++, Python & JS",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ffffff] text-base sm:text-lg mb-6 lg:text-xl">
+            i´m making my first web site with webcoded tutorial
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-300 to-indigo-600 hover:bg-slate-200 text-white">
+              Hire me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br  from-blue-300 to-indigo-600 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Go To CV
+              </span>
+            </button>
+          </div>
         </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative overflow-hidden">
+            <Image
+              src="/images/hero.png"
+              alt="hero"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
+      </div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-      <main className="flex main-h-screen flex-cool bg-[#000000] container mx-auto">
+    <main className="flex main-h-screen flex-cool bg-[#121212]">
+      <div className="container mx-auto px-12 py-4">
         <HeroSection />
-      </main>
+      </div>
+    </main>
   );
 }
